392706caf5d00d683b4a9e4e4722c61cde04d140
saurabh-Sharma44 <sau02xy@gmail.com>
2024-05-29 13:18:24 +0530

    doubly linkrlt modyfied

---
+++ doublylinkedlist.c
@@ -0,0 +1,39 @@
+#include<stdio.h>
+#include<stdlib.h>
+ struct node{
+    int data;
+    struct node *next;
+    struct node *prev;
+
+};
+//creating a pointer to the head and tail
+struct node*head = NULL;
+struct node*tail = NULL;
+//creating new node
+struct node* create_node(int data)
+{
+    struct node *new_node;
+    new_node = malloc(sizeof(struct node));
+    new_node ->data = data;
+    new_node ->next = NULL;
+    new_node ->prev = NULL;
+    return new_node;
+
+}
+//inserting a node at a begining
+void insert_at_beg(int data){
+     if(head==NULL){
+        head = new_node;
+
+     }
+}
+int main(){
+    insert_at_beg(19);
+    insert_at_end(14);
+    insert_at_beg(19);
+    insert_at_beg(19);
+    insert_at_end(14);
+    delete_at_beg();
+    delete_at_end();
+
+}
